8d775f30ac6660fdcd2d0c4b3c01dd5d06d87696
echu-mohan <156306645+echu-mohan@users.noreply.github.com>
2024-12-03 14:48:23 +0100

    created the react project

---
+++ taskhive/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Task Planner</h1>
+        <p>It's going to change zour Life</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
